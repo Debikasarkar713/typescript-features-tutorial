72409728d37bc44c41e5728e711cf6fa45c1bbfc
debika sarkar <debikasarkar713@gmail.com>
2019-10-13 17:55:57 -0400

    Add examples of interfaces in features

--- annotations/interfaces.ts
+++ annotations/interfaces.ts
@@ -1,20 +1,29 @@
 //when creating interface use capital letter
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  summary(): string;
 }
 
+//because they are both of type Reportable they will both work.
 const oldCivic = {
   name: 'civic',
-  year: 2000,
-  broken: true
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+};
+const drinkItem = {
+  color: 'brown',
+  carbonated: true,
+  sugarContent: 40,
+  summary(): string {
+    return `My drink has ${this.sugarContent}`;
+  }
+};
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
 };
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken: ${vehicle.broken}`);
-};
-
-printVehicle(oldCivic);
+printSummary(oldCivic);
+printSummary(drinkItem);
+//we can use a single interface to describe very different objects
